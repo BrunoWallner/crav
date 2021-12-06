902c8581ff43e1a24eb1b75d9bc39d39ecdc4436
Luca <b.lucab1211@gmail.com>
2021-12-06 14:52:42 +0100

    fixed broken compilation on windows crossterm backend

--- src/backends/crossterm/mod.rs
+++ src/backends/crossterm/mod.rs
@@ -1,6 +1,7 @@
 use crate::config::{Config, Color};
+use audioviz::spectralizer::stream::StreamController;
 
-pub fn run(mut _config: &mut Config, _audio: audioviz::AudioStream, _color_modes: Vec<Color>) {
+pub fn run(mut _config: &mut Config, _audio_controller: StreamController) {
     println!("The Windows terminal is currently not supported, as it does not support ANSI escape codes");
     println!("please use the wgpu backend with the '--backend wgpu' parameter");
 }
